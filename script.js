75ae620814365b0ec862d7560efa79857d2f4d22
Vigofac <83132659+Vigofac@users.noreply.github.com>
2026-03-29 08:26:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+window.onload = () => {
+  document.querySelectorAll(".progress div").forEach(bar => {
+    let w = bar.style.width;
+    bar.style.width = "0";
+    setTimeout(() => bar.style.width = w, 300);
+  });
+};
